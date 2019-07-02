40eb3ed173e2e85c143b0853b8c75c2125b21e3c
Lynne <lynne@bune.city>
2019-07-02 20:44:48 +1000

    removed unused imports

--- gen.py
+++ gen.py
@@ -4,7 +4,7 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 from mastodon import Mastodon
-import argparse, sys, traceback, json, re
+import argparse, json, re
 import functions
 
 parser = argparse.ArgumentParser(description='Generate and post a toot.')
