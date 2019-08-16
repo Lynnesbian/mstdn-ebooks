910829d9464a9ef5f0365b4df3e57e47d166a7d8
Lynne <lynne@bune.city>
2019-08-16 13:50:47 +1000

    don't insert duplicate posts

--- main.py
+++ main.py
@@ -220,6 +220,7 @@ for f in following:
 							#we've caught up to the notices we've already downloaded, so we can stop now
 							#you might be wondering, "lynne, what if the instance ratelimits you after 40 posts, and they've made 60 since main.py was last run? wouldn't the bot miss 20 posts and never be able to see them?" to which i reply, "i know but i don't know how to fix it"
 							done = True
+							continue
 					if 'lang' in cfg:
 						try:
 							if oi['object']['contentMap'][cfg['lang']]: # filter for language
